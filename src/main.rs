9e724d77dd58bc7dbc78496ef5bd9b0e5bba7ae4
Kyllingene <fuzzmuffin343@gmail.com>
2023-05-22 12:42:47 -0700

    Even more functional

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::{error::Error, fmt::Display};
 
-type CompResult<'a, T> = Result<T, CompError>;
+type CompResult<T> = Result<T, CompError>;
 
 macro_rules! s {
     ( $s:expr ) => {
@@ -15,7 +15,7 @@ enum CompError {
     InvalidOp(usize, String),
     RequireArg(usize, String),
     UnbalancedLoops,
-    Other(usize),
+    // Other(usize),
 }
 
 impl Display for CompError {
@@ -26,14 +26,58 @@ impl Display for CompError {
                 write!(f, "Operator `{op}` on line {i} requires an argument")
             }
             CompError::UnbalancedLoops => write!(f, "Program loop starts != loop ends"),
-            CompError::Other(i) => write!(f, "Miscellaneous error on line {i}"),
+            // CompError::Other(i) => write!(f, "Miscellaneous error on line {i}"),
         }
     }
 }
 
 impl Error for CompError {}
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+fn goto(n: usize) -> String {
+    format!(
+        "<<<<<[<<<<]>{}",
+        ">".repeat(n * 4)
+    )
+}
+
+fn gotoi(i: u8) -> String {
+    format!(
+        "<<<<<[<<<<]>>>>>>>{0}[{1}>>>>{0}]{1}<<",
+        "-".repeat(i as usize),
+        "+".repeat(i as usize)
+    )
+}
+
+fn goback() -> String {
+    s!("<[>>>>]>")
+}
+
+fn gohome() -> String {
+    s!("<[<<<<]>")
+}
+
+// fn find(i: u8) -> String {
+//     /* while x != y: [x[-y-x]+y[x-y[-]]x-]
+//        clears x* and y
+
+//     x = dup cell.tag
+//     y = dup target_tag
+//     while x != y {
+//         >>>>
+//         x = dup cell.tag
+//         y = dup target_tag
+//     }
+    
+//      * x may be 1
+//     */
+
+//     format!(
+//         "",
+//         goback(),
+//     )
+// }
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Op {
     // math
     /// Push pop + pop.
@@ -67,8 +111,21 @@ enum Op {
     /// Rotate the top three values (i.e. 1, 2, 3 => 2, 3, 1).
     Rot,
 
-    /// Swap the top value and value N backwards.
+    /// Swap the top value and the Nth value backwards.
+    Swpb(usize),
+
+    /// Swap the top value and the Nth value.
     Swpn(usize),
+
+    /// Assign the tag in the top value (not popped) to the top cell.
+    Tag(Option<u8>),
+
+    // TODO: allow dynamic tag finding
+    /// Overwrite the top value with the cell at tag.
+    Ctg(u8),
+
+    /// Pop and write into cell at tag.
+    Mtg(u8),
 
     // flow control
     /// Skip the next instruction if pop == 0.
@@ -87,75 +144,83 @@ enum Op {
 
     /// Push a byte from the input stream (i.e. stdin).
     In,
+
+    /// A raw brain* string
+    Raw(String),
 }
 
 impl Op {
-    fn to_str(&self, start: usize) -> String {
+    fn to_str(&self) -> String {
         match self {
-            Op::Add => s!("[-<+>]<"),
-            Op::Sub => s!("[-<->]<"),
-            Op::Mul => s!("<[->>+<<]>[->[->+<<<+>>]>[-<+>]<<]>[-]<<"),
+            Op::Add => s!("[-<<<<+>>>>]<-<<<"),
+            Op::Sub => s!("[-<<<<->>>>]<-<<<"),
+            // Op::Mul => s!("<[->>+<<]>[->[->+<<<+>>]>[-<+>]<<]>[-]<<"),
+            Op::Mul => todo!(),
             Op::Div => todo!(),
-            Op::Push(arg) => s!(">[-]") + &"+".repeat(*arg as usize),
+            Op::Push(arg) => s!(">>>+>[-]") + &"+".repeat(*arg as usize),
             Op::Set(arg) => s!("[-]") + &"+".repeat(*arg as usize),
-            Op::Pop => s!("[-]<"),
-            Op::Dup => s!(">[-]>[-]<<[->+>+<<]>>[-<<+>>]<"),
-            // a b b
-            Op::Swp => format!("{}<[-]<[->+<]>>[-<<+>>]<", Op::Dup.to_str(start)),
-            Op::Rot => format!("<{}>{}", Op::Swp.to_str(start - 1), Op::Swp.to_str(start)),
-            Op::Swpn(i) => format!(
+            Op::Pop => s!("[-]<-<<<"),
+            Op::Dup => s!(">[-]>>+>[-]<<<<[->+>>>+<<<<]>[-<+>]>>>"),
+            Op::Swp => s!(">[-]<[->+<]<<<<[->>>>+<<<<]>>>>>[-<<<<<+>>>>>]<"),
+            Op::Rot => format!("<<<<{}>>>>{}", Op::Swp, Op::Swp),
+            Op::Swpb(i) => format!(
                 "{}<[-]{1}[-{2}+{1}]>{2}[-<{1}+>{2}]<",
-                Op::Dup.to_str(start),
+                Op::Dup,
                 "<".repeat(*i),
                 ">".repeat(*i),
+            ),
+            Op::Swpn(n) => format!(
+                "<->>[-]<[->+<]{0}[-{1}+{0}]{1}>[-<{0}+{1}>]<<+>",
+                goto(*n),
+                goback(),
+            ),
+            Op::Tag(i) => {
+                if let Some(i) = i {
+                    format!(">>{}<<", Op::Set(*i))
+                } else {
+                    format!("[->+>+<<]>[-<+>]<")
+                }
+            },
+            Op::Ctg(i) => format!(
+                "<->>[-]<[->+<]{0}[-{1}+{0}]{1}>[-<{0}+{1}>]<<+>",
+                gotoi(*i),
+                goback(),
+            ),
+            Op::Mtg(i) => format!(
+                "{0}[-]{1}[-{0}+{1}]<-<<<",
+                gotoi(*i),
+                goback(),
             ),
             Op::Skip => todo!(),
             Op::Pass => todo!(),
             Op::Loop => s!("["),
             Op::End => s!("]"),
-            Op::Out => s!(".[-]<"),
-            Op::In => s!(">[-],"),
+            Op::Out => s!(".[-]<-<<<"),
+            Op::In => s!(">>>+>[-],"),
+            Op::Raw(code) => code.clone(),
         }
     }
+}
 
-    fn end_pos(&self, start: usize) -> usize {
-        match self {
-            Op::Add => start - 1,
-            Op::Sub => start - 1,
-            Op::Mul => start - 1,
-            Op::Div => start - 1,
-            Op::Push(_) => start + 1,
-            Op::Set(_) => start,
-            Op::Pop => start - 1,
-            Op::Dup => start + 1,
-            Op::Swp => start,
-            Op::Rot => start,
-            Op::Swpn(_) => start,
-            Op::Skip => start - 1,
-            Op::Pass => start - 1,
-            Op::Loop => start,
-            Op::End => start,
-            Op::Out => start - 1,
-            Op::In => start - 1,
-        }
+impl Display for Op {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_str())
     }
 }
 
 pub struct Compiler;
 
 impl Compiler {
-    fn from_tokens(tokens: &Vec<Op>) -> CompResult<String> {
+    fn from_tokens(tokens: Vec<Op>) -> CompResult<String> {
         if tokens.iter().filter(|op| **op == Op::Loop).count()
             != tokens.iter().filter(|op| **op == Op::End).count()
         {
             return Err(CompError::UnbalancedLoops);
         }
 
-        let mut data = String::new();
-        let mut pos = 0;
+        let mut data = String::from(">");
         for tok in tokens {
-            pos = tok.end_pos(pos);
-            data.push_str(&tok.to_str(pos));
+            data.push_str(&tok.to_str());
         }
 
         Ok(data)
@@ -163,14 +228,18 @@ impl Compiler {
 
     fn from_str(code: &str) -> CompResult<String> {
         let mut toks = Vec::new();
-        for (i, mut line) in code.lines().enumerate() {
-            if line.starts_with(';') || line.is_empty() || line.chars().all(|ch| ch.is_whitespace())
-            {
-                continue;
-            }
+        for (i, line) in code.lines().enumerate() {
+            let mut line = line.trim();
 
-            if let Some(l) = line.split_once(';') {
-                line = l.0;
+            if !line.starts_with("raw") {
+                if line.starts_with(';') || line.is_empty() || line.chars().all(|ch| ch.is_whitespace())
+                {
+                    continue;
+                }
+
+                if let Some(l) = line.split_once(';') {
+                    line = l.0;
+                }
             }
 
             let line = line.trim().to_lowercase();
@@ -192,7 +261,27 @@ impl Compiler {
                 "pop" => toks.push(Op::Pop),
                 "dup" => toks.push(Op::Dup),
                 "swp" => toks.push(Op::Swp),
+                "swpb" => toks.push(Op::Swpb(
+                    arg.parse()
+                        .map_err(|_| CompError::RequireArg(i, op.to_owned()))?,
+                )),
                 "swpn" => toks.push(Op::Swpn(
+                    arg.parse()
+                        .map_err(|_| CompError::RequireArg(i, op.to_owned()))?,
+                )),
+                "tag" => toks.push(Op::Tag(
+                    if arg.is_empty() {
+                        None
+                    } else {
+                        Some(arg.parse()
+                            .map_err(|_| CompError::RequireArg(i, op.to_owned()))?)
+                    }
+                )),
+                "ctg" => toks.push(Op::Ctg(
+                    arg.parse()
+                        .map_err(|_| CompError::RequireArg(i, op.to_owned()))?,
+                )),
+                "mtg" => toks.push(Op::Mtg(
                     arg.parse()
                         .map_err(|_| CompError::RequireArg(i, op.to_owned()))?,
                 )),
@@ -203,11 +292,12 @@ impl Compiler {
                 "end" => toks.push(Op::End),
                 "out" => toks.push(Op::Out),
                 "in" => toks.push(Op::In),
+                "raw" => toks.push(Op::Raw(arg.to_owned())),
                 _ => return Err(CompError::InvalidOp(i, op.to_owned())),
             }
         }
 
-        Self::from_tokens(&toks)
+        Self::from_tokens(toks)
     }
 }
 
